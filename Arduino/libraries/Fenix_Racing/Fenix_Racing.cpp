80f79cbfd33ad0ec9f40ccf3863eee0a4d828e18
Rhomulo <rhomulothiago@gmail.com>
2024-01-02 16:22:31 -0300

    2/1/24

--- Arduino/libraries/Fenix_Racing/Fenix_Racing.cpp
+++
@@ -1,9 +0,0 @@
-#include "Fenix_Racing.h"
-#include "Arduino.h"
-
-
-
-void armazenarBytes(int valor, byte *dataArray, int startIndex) {
-dataArray[startIndex] = highByte(valor);
-dataArray[startIndex + 1] = lowByte(valor);
-}
--- Arduino/libraries/Fenix_Racing/Fenix_Racing.h
+++
@@ -1,7 +0,0 @@
-#ifndef FENIX_RACING_H
-#define FENIX_RACING_H
-
-#include "Arduino.h"
-
- void armazenarBytes(int value, byte *dataArray, int startIndex);
-#endif
